5a67d2fc7dc4ffa2636a49ff49a5e5d0fe0d50c3
YPeng11 <161717186+YPeng11@users.noreply.github.com>
2025-10-05 21:47:04 +0800

    search&category api

--- API/api-controller.js
+++ API/api-controller.js
@@ -45,12 +45,67 @@ router.get("/detail/:id", (req, res) => {
 		WHERE events.id = ?
 		`, eventId, (err, records, fields) => {
 			if (err) {
-				console.error("Error while retrieve the data");
+				console.error("Error while retrieve the event detail data");
 			} else {
 				res.json(records[0]);
 			}
 		})
 })
+
+router.get("/search", (req, res) => {
+    const { date, location, category_id } = req.query;
+    
+    let sqlQuery = `
+        SELECT events.*, 
+            organization.name as organization_name,
+            organization.email as organization_email,
+            categories.name as category_name
+        FROM events 
+        LEFT JOIN organization ON events.organizer_id = organization.id
+        LEFT JOIN categories ON events.category_id = categories.id
+        WHERE events.ban_status = 1
+    `;
+    
+    const queryParams = [];
+    
+    if (date) {
+        sqlQuery += ` AND events.date = ?`;
+        queryParams.push(date);
+    }
+    
+    if (location) {
+        sqlQuery += ` AND events.Location LIKE ?`;
+        queryParams.push(`%${location}%`);
+    }
+    
+    if (category_id) {
+        sqlQuery += ` AND events.category_id = ?`;
+        queryParams.push(category_id);
+    }
+    
+    connection.query(sqlQuery, queryParams, (err, records, fields) => {
+        if (err) {
+			console.error("Error while search");
+        } else {
+           res.json(records);
+        }
+    });
+});
+
+
+
+router.get("/categories", (req, res) => {
+    connection.query(`
+        SELECT * FROM categories
+    `, (err, records, fields) => {
+        if (err) {
+            console.error("Error while retrieve the category data:", err);
+        } else {
+            res.json(records);
+        }
+    });
+});
+
 
 
 module.exports = router;
